5507db04296d3303c8f18752b519a4e45bfe36f9
SSSergiy <sergey1991serg@gmail.com>
2022-10-21 00:28:49 +0300

    oien

---
+++ js/lason16.js
@@ -0,0 +1,13 @@
+function pow(num, degree) {
+  if (degree === 1) {
+    return num;
+	} if (degree === 0) {
+	return num
+	}
+  return num * pow(num, degree - 1);
+}
+console.log(pow(3, 2));
+
+
+
+

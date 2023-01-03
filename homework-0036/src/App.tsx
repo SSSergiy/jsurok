99027b3e173585dcac5806693913c927ec528572
SSSergiy <sergey1991serg@gmail.com>
2023-01-03 23:51:35 +0200

    add Clas

--- homework-0036/src/App.tsx
+++ homework-0036/src/App.tsx
@@ -1,34 +1,10 @@
 import "./App.css";
+import Smiles from "./components/Smiles/Smiles";
 
 function App() {
-    const count = 0;
-
   return (
     <>
-      <div className="container">
-        <div className="container">
-          <div>
-            <button onClick={()=> count +1}>👍</button>
-            <div className="counter">{count}</div>
-          </div>
-          <div>
-            <button>👎</button>
-            <div className="counter">0</div>
-          </div>
-          <div>
-            <button>💚</button>
-            <div className="counter">0</div>
-          </div>
-          <div>
-            <button>💩</button>
-            <div className="counter">0</div>
-          </div>
-          <div>
-            <button>🤣</button>
-            <div className="counter">0</div>
-          </div>
-        </div>
-      </div>
+   <Smiles />
     </>
   );
 }

7efb9804cdd777e62a53109a7b86aca810ef3d2d
SSSergiy <sergey1991serg@gmail.com>
2023-01-11 05:30:43 +0200

    add button modal functional from liborary

--- homework-0038/src/components/ButtonModal/ButtonModal.tsx
+++ homework-0038/src/components/ButtonModal/ButtonModal.tsx
@@ -1,13 +1,16 @@
-import {FC} from "react";
+import { FC } from 'react';
+import { Button, Space } from 'antd';
 
 const ButtonModal: FC = ({ openModal }) => {
-	// console.log(openModal);
-	
+  // console.log(openModal);
+
   return (
     <>
-      <button type='button' onClick={openModal}>
-        <span>Add Character</span>
-      </button>
+      <Space direction='vertical' style={{ width: '10%' }}>
+        <Button type='primary' block htmlType='button' onClick={openModal}>
+          <span>Add Character</span>
+        </Button>
+      </Space>
     </>
   );
 };

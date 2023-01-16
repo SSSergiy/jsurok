03ede74f637bbdbd71dba6f31ddd4d4d4d8b4ef6
SSSergiy <sergey1991serg@gmail.com>
2023-01-16 02:56:44 +0200

    refacting editorconfig

--- homework-0039/src/App.tsx
+++ homework-0039/src/App.tsx
@@ -1,12 +1,12 @@
 // import Table from './components/Table';
-import { StrictMode } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Users from './components/pages/Users';
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Users from "./components/pages/Users";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Users/>
+    path: "/",
+    element: <Users />,
   },
 ]);
 
--- homework-0039/src/components/pages/Users.tsx
+++ homework-0039/src/components/pages/Users.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import NavigationIcon from '@mui/icons-material/Navigation';
+import { useEffect, useState } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import NavigationIcon from "@mui/icons-material/Navigation";
 import {
-	AppBar,
-	Box,
-	Toolbar,
+  AppBar,
+  Box,
+  Toolbar,
   TableContainer,
   Table,
   TableHead,
@@ -18,8 +18,8 @@ import {
   AccordionDetails,
   Typography,
   TableFooter,
-  Fab
-} from '@mui/material';
+  Fab,
+} from "@mui/material";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
@@ -27,7 +27,7 @@ const Users = () => {
   const [getAlbums, sepAlbums] = useState(false);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => {
         setUsers(users);
@@ -36,7 +36,7 @@ const Users = () => {
 
   const [albums, setAlbums] = useState([]);
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/albums')
+    fetch("https://jsonplaceholder.typicode.com/albums")
       .then((res) => res.json())
       .then((albums) => {
         setAlbums(albums);
@@ -45,7 +45,7 @@ const Users = () => {
 
   const [photos, setPhotos] = useState([]);
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/photos')
+    fetch("https://jsonplaceholder.typicode.com/photos")
       .then((res) => res.json())
       .then((photos) => {
         setPhotos(photos);
@@ -53,16 +53,14 @@ const Users = () => {
       });
   }, []);
 
-
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
       backgroundColor: theme.palette.primary.light,
-      color: theme.palette.common.white
+      color: theme.palette.common.white,
     },
     [`&.${tableCellClasses.body}`]: {
-      fontSize: 14
-    }
-    // console.log(theme.palette.common)
+      fontSize: 14,
+    },
   }));
   function stringToColor(string: string) {
     let hash = 0;
@@ -70,81 +68,81 @@ const Users = () => {
     for (i = 0; i < string.length; i += 1) {
       hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
-    let color = '#';
+    let color = "#";
     for (i = 0; i < 3; i += 1) {
       const value = (hash >> (i * 8)) & 0xff;
       color += `00${value.toString(16)}`.slice(-2);
     }
     return color;
   }
-  function stringAvatar(name:string) {
+  function stringAvatar(name: string) {
     return {
       sx: {
-        bgcolor: stringToColor(name)
+        bgcolor: stringToColor(name),
       },
-      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
     };
   }
 
-  const filterAlbumsFunk = (id:number)=> {
-    const arr = albums.filter((item):boolean => {
+  const filterAlbumsFunk = (id: number) => {
+    const arr = albums.filter((item): boolean => {
       return item.userId === id;
     });
     return arr;
   };
 
-  const filterPhotosFunk = (id:number) => {
+  const filterPhotosFunk = (id: number) => {
     const arr = photos.filter((item) => {
       return item.albumId === id;
     });
     return arr;
   };
 
-  const lookPhotos = ():void => {
+  const lookPhotos = (): void => {
     sepPhotos(true);
   };
-  const lookAlbums = ():void => {
+  const lookAlbums = (): void => {
     sepPhotos(false);
     sepAlbums(true);
-	};
-	const removAlbums = ():void => {
-		sepPhotos(false);
-		sepAlbums(false);
-	}
+  };
+  const removAlbums = (): void => {
+    sepPhotos(false);
+    sepAlbums(false);
+  };
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position='static'>
+        <AppBar position="static">
           <Toolbar
-            variant='dense'
+            variant="dense"
             sx={{
-              display: 'flex',
-              justifyContent: 'space-around',
-              padding: '30px'
+              display: "flex",
+              justifyContent: "space-around",
+              padding: "30px",
             }}
           >
-            <Fab variant='extended'onClick={lookAlbums}>
-              <NavigationIcon  />
-             ALBOMS
+            <Fab variant="extended" onClick={lookAlbums}>
+              <NavigationIcon />
+              ALBOMS
             </Fab>
-            <Fab variant='extended'onClick={removAlbums}>
-              <NavigationIcon  />
-             Remowe ALL action
+            <Fab variant="extended" onClick={removAlbums}>
+              <NavigationIcon />
+              Remowe ALL action
             </Fab>
-            <Fab variant='extended'onClick={lookPhotos} >
-              <NavigationIcon  />
+            <Fab variant="extended" onClick={lookPhotos}>
+              <NavigationIcon />
               PHOTOS
             </Fab>
           </Toolbar>
         </AppBar>
       </Box>
-      <main className='container'>
+      <main className="container">
         {users.map(({ name, username, phone, website, id }) => (
           <Accordion>
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
-              aria-controls='panel1a-content'
-              id='panel1a-header'
+              aria-controls="panel1a-content"
+              id="panel1a-header"
             >
               <TableContainer>
                 <Table>
@@ -165,18 +163,18 @@ const Users = () => {
                         </Typography>
                       </StyledTableCell>
                       <StyledTableCell>
-                        <Typography variant='h6'>{name}</Typography>
+                        <Typography variant="h6">{name}</Typography>
                       </StyledTableCell>
                       <StyledTableCell>
-                        <Typography variant='h6'>{username}</Typography>
+                        <Typography variant="h6">{username}</Typography>
                       </StyledTableCell>
                       <StyledTableCell>
-                        <Typography variant='h6'>
-                          {phone.split('x')[0]}
+                        <Typography variant="h6">
+                          {phone.split("x")[0]}
                         </Typography>
                       </StyledTableCell>
                       <StyledTableCell>
-                        <Typography variant='h6'>{website}</Typography>
+                        <Typography variant="h6">{website}</Typography>
                       </StyledTableCell>
                     </TableRow>
                   </TableFooter>
@@ -190,11 +188,11 @@ const Users = () => {
                     <div>
                       <div
                         style={{
-                          border: '4px ridge rgba(211, 220, 50, .6)',
-                          padding: '5px'
+                          border: "4px ridge rgba(211, 220, 50, .6)",
+                          padding: "5px",
                         }}
                       >
-                        {' '}
+                        {" "}
                         {item.title}
                       </div>
                       {getPhotos &&

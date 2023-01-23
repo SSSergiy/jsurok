aab034914f7c122879e2a28a8720be9fa1ad441b
SSSergiy <sergey1991serg@gmail.com>
2023-01-23 04:07:06 +0200

    oei

--- homework-0039_01/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "@picocss/pico";
-createRoot(document.getElementById("root") as HTMLElement).render(<App />);
